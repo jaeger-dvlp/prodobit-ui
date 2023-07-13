01563585b103a72a5a247476c39ef51fca5fabf1
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-13 17:20:02 +0300

    rev: theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foundationgreen: [
+          '#EEFCF9',
+          '#DDF5F0',
+          '#B6EDE0',
+          '#8CE4D0',
+          '#6CDDC2',
+          '#59D8B8',
+          '#4DD6B4',
+          '#3EBD9E',
+          '#32A88C',
+          '#1C9278',
+        ],
+      },
+    },
   },
   plugins: [],
 };
