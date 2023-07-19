3f7171afb70ce95ba91a8adb6e13030f3127d171
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-19 17:56:33 +0300

    feat: sub-menu

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Box, Image } from '@mantine/core';
+import { NavLink, PathMatch } from 'react-router-dom';
+import isRouteActive from '@/common/hooks/isRouteActive';
 import MainRouteLink from '@/components/sidebar/MainRouteLink';
+import { AnimatePresence, Variant, motion } from 'framer-motion';
 import SidebarProfile from '@/components/sidebar/SidebarProfile';
 import RouteLeftIndicator from '@/components/sidebar/RouteLeftIndicator';
+import { Box, Button, CSSObject, Image, MantineTheme } from '@mantine/core';
 
 import {
   TasksIcon,
@@ -15,9 +18,59 @@ const Images = {
   logo: '/assets/img/logo.svg',
 };
 
+const SubRouteAnims: {
+  [key: string]: Variant;
+} = {
+  hidden: {
+    opacity: 0,
+    y: -10,
+    height: 0,
+    marginTop: 0,
+    transition: {
+      duration: 0.15,
+    },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    height: 'fit-content',
+    marginTop: 30,
+    transition: {
+      duration: 0.15,
+    },
+  },
+};
+
 function Routes() {
+  const SecondarySubRouteStyles = (
+    theme: MantineTheme,
+    isActive?: PathMatch<string> | null,
+  ): CSSObject => ({
+    border: 'none',
+    display: 'flex',
+    fontWeight: 500,
+    fontSize: '20px',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative',
+    height: 'fit-content',
+    justifyContent: 'start',
+    transition: 'all 0.15s ease-in-out',
+    padding: `20px 30px 20px ${isActive ? 30 : 0}px`,
+    color: isActive ? theme.colors.foundationgreen[9] : '#676668',
+    backgroundColor: isActive ? theme.colors.foundationgreen[1] : 'transparent',
+    ':hover': {
+      color: theme.colors.foundationgreen[9],
+      backgroundColor: isActive
+        ? theme.colors.foundationgreen[1]
+        : 'transparent',
+    },
+  });
+
   return (
     <Box
+      className="sidebar-routes"
       component="ul"
       sx={(theme) => ({
         margin: 0,
@@ -44,6 +97,65 @@ function Routes() {
           />
           <span>Dashboard</span>
         </MainRouteLink>
+        <AnimatePresence>
+          {isRouteActive('/dashboard') && (
+            <Box
+              exit="hidden"
+              initial="hidden"
+              animate="visible"
+              className="sub-routes"
+              component={motion.ul}
+              variants={SubRouteAnims}
+              sx={{
+                listStyle: 'none',
+                position: 'relative',
+                margin: '30px 0px 0px 35px',
+              }}
+            >
+              <Box component="li">
+                <Button
+                  to="/dashboard/sales"
+                  variant="default"
+                  component={NavLink}
+                  sx={(theme) =>
+                    SecondarySubRouteStyles(
+                      theme,
+                      isRouteActive('/dashboard/sales'),
+                    )
+                  }
+                >
+                  Sales
+                </Button>
+                <Button
+                  to="/dashboard/general-analysis"
+                  variant="default"
+                  component={NavLink}
+                  sx={(theme) =>
+                    SecondarySubRouteStyles(
+                      theme,
+                      isRouteActive('/dashboard/general-analysis'),
+                    )
+                  }
+                >
+                  Gen. Analysis
+                </Button>
+                <Button
+                  to="/dashboard/performance"
+                  variant="default"
+                  component={NavLink}
+                  sx={(theme) =>
+                    SecondarySubRouteStyles(
+                      theme,
+                      isRouteActive('/dashboard/performance'),
+                    )
+                  }
+                >
+                  Performance
+                </Button>
+              </Box>
+            </Box>
+          )}
+        </AnimatePresence>
       </Box>
       <Box component="li">
         <MainRouteLink to="/analyzes">
@@ -96,9 +208,9 @@ function Sidebar() {
       sx={(theme) => ({
         padding: 0,
         width: '100%',
+        height: '100vh',
         display: 'flex',
         maxWidth: '400px',
-        minHeight: '100%',
         alignItems: 'start',
         position: 'relative',
         flexDirection: 'column',
