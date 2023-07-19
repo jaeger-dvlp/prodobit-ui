3fd171e4c005ba128b2f33a17335a1918c9d4690
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-19 13:22:21 +0300

    style: font-family & breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: { xs: '30em', sm: '48em', md: '64em', lg: '74em', xl: '90em' },
     extend: {
       colors: {
         foundationgreen: [
