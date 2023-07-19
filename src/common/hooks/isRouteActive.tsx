cab4f5339a29721e9451ecd69200bec74a21f6b5
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-19 23:31:39 +0300

    rm: unused hooks

--- src/common/hooks/isRouteActive.tsx
+++
@@ -1,7 +0,0 @@
-import { useMatch } from 'react-router-dom';
-
-const isRouteActive = (route: string) => {
-  return useMatch(`${route.endsWith('/') ? route : `${route}/`}*`);
-};
-
-export default isRouteActive;
