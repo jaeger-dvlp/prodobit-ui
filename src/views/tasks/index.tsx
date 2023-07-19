d70ed91fc236b0406454cd7c445a73e039689407
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-19 13:22:59 +0300

    feat: create as empty

---
+++ src/views/tasks/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box } from '@mantine/core';
+import { motion } from 'framer-motion';
+
+function Tasks() {
+  return (
+    <Box
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      component={motion.div}
+      sx={(theme) => ({
+        width: '100%',
+        minHeight: '100%',
+        padding: theme.spacing.xl,
+        backgroundColor: 'transparent',
+        [theme.fn.smallerThan('md')]: {
+          borderRadius: 0,
+          backgroundColor: '#fff',
+        },
+      })}
+    >
+      Tasks
+    </Box>
+  );
+}
+
+export default Tasks;
