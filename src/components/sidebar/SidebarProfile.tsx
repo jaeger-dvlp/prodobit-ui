3f339b06807b10f95cd0c57363be0382c504cd23
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-21 11:05:50 +0300

    style: minimal fixes

--- src/components/sidebar/SidebarProfile.tsx
+++ src/components/sidebar/SidebarProfile.tsx
@@ -67,7 +67,6 @@ function SidebarProfile() {
             gap: 0,
             width: '100%',
             display: 'flex',
-
             alignItems: 'center',
             flexDirection: 'row',
             justifyContent: 'space-between',
