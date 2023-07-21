ce34517639869d2d78b32592b652f25fe4fd364b
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-21 11:06:26 +0300

    rev: with buttons

--- src/views/items/index.tsx
+++ src/views/items/index.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
-import { Box } from '@mantine/core';
+import { Box, Text } from '@mantine/core';
 import { motion } from 'framer-motion';
 import RoutesMap, { RouteMapItem } from '@/routes';
 import NavbarWBreadCrumbs from '@/components/layout/navbar/withBreadCrumbs';
 
+function ItemCountDisplay({ itemCount }: { itemCount: number }) {
+  if (itemCount === 0) {
+    return (
+      <Text
+        sx={(theme) => ({
+          color: '#39373A',
+          fontSize: '22px',
+          fontWeight: 400,
+          [theme.fn.smallerThan('md')]: {
+            fontSize: '18px',
+          },
+        })}
+        miw="fit-content"
+      >
+        Öğe Bulunamadı
+      </Text>
+    );
+  }
+
+  return (
+    <Text
+      sx={(theme) => ({
+        color: '#39373A',
+        fontSize: '22px',
+        fontWeight: 400,
+        [theme.fn.smallerThan('md')]: {
+          fontSize: '18px',
+        },
+      })}
+      miw="fit-content"
+    >
+      <Text component="b">{itemCount}</Text> Öğe Bulundu
+    </Text>
+  );
+}
+
 function Items() {
+  const [
+    itemCount,
+    // ,setItemCount
+  ] = React.useState(2500);
+
   const Route = RoutesMap.find(
     (route: RouteMapItem) => route.path === '/items',
   );
@@ -37,6 +78,8 @@ function Items() {
           path: route?.path,
           name: route?.name || '?',
         }))}
+        withButtons
+        middleChilds={<ItemCountDisplay itemCount={itemCount} />}
       />
     </Box>
   );
