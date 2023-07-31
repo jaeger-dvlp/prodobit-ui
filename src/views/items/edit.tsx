744e238f4a53b1c9a2baba94e3187cbe69240959
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-31 15:11:26 +0300

    feat: functional navbar

--- src/views/items/edit.tsx
+++ src/views/items/edit.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { MockItems } from 'mockdata';
 import { Item } from '@/views/items/list/WithItems';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Box, Button, Sx, Text } from '@mantine/core';
+import Navbar from '@/components/layout/Navbar';
+import { motion } from 'framer-motion';
+import RoutesMap, { RouteMapItem } from '@/routes';
+import { ProdobitAppTheme as t } from '@/theme';
+import { ImageIcon, InfoIcon } from '@/components/icons';
 
 function EditItem() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { state } = useLocation();
   const [item, setItem] = React.useState<Item | null>(null);
+  const [view, setView] = React.useState<'info' | 'docs'>('info');
   const routeItem = state?.item;
 
   React.useEffect(() => {
@@ -27,7 +34,92 @@ function EditItem() {
     })();
   }, [id, navigate, routeItem]);
 
-  return <div>edit {item?.name}</div>;
+  const Route = RoutesMap.find((route: RouteMapItem) => route.path === '/items');
+  const SubRoute = Route?.subRoutes?.find((route: RouteMapItem) => route.path === '/items/edit');
+
+  const getViewSx = (viewName: string): Sx => ({
+    gap: '10px',
+    height: 'auto',
+    border: 'none',
+    display: 'flex',
+    fontWeight: 500,
+    fontSize: '15px',
+    borderRadius: 100,
+    lineHeight: '18px',
+    padding: '9px 18px',
+    flexDirection: 'row',
+    alignContent: 'center',
+    color: t.colors.gray[8],
+    justifyContent: 'center',
+    opacity: view === viewName ? 1 : 0.6,
+    transition: 'all 200ms ease-in-out',
+    backgroundColor: view === viewName ? t.colors.gray[3] : 'transparent',
+    ':hover': {
+      backgroundColor: view === viewName ? t.colors.gray[3] : t.colors.gray[2],
+    },
+  });
+
+  return (
+    <Box
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      component={motion.section}
+      sx={(theme) => ({
+        padding: 60,
+        width: '100%',
+        minHeight: '100%',
+        backgroundColor: 'transparent',
+        [theme.fn.smallerThan('md')]: {
+          padding: 45,
+          paddingTop: 80,
+        },
+      })}
+    >
+      <Navbar
+        paths={[
+          Route,
+          {
+            ...SubRoute,
+            path: `${SubRoute?.path}/${item?.id}`,
+          },
+        ].map((route) => ({
+          path: route?.path,
+          name: route?.name || '?',
+        }))}
+        withButtons
+        middleChilds={
+          <Box
+            sx={{
+              gap: 1,
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              minWidth: 'fit-content',
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              variant="default"
+              onClick={() => setView('info')}
+              sx={(() => getViewSx('info'))()}
+            >
+              <InfoIcon width={16} height={16} />
+              <Text ml={10}>Bilgiler</Text>
+            </Button>
+            <Button
+              variant="default"
+              onClick={() => setView('docs')}
+              sx={(() => getViewSx('docs'))()}
+            >
+              <ImageIcon width={16} height={16} />
+              <Text ml={10}>Dökümanlar</Text>
+            </Button>
+          </Box>
+        }
+      />
+    </Box>
+  );
 }
 
 export default EditItem;
