e484d71e4fd97f448917e5adec88d864b3593055
JAEGER_ <omer.kayalar0101@icloud.com>
2023-07-31 13:54:43 +0300

    feat: handle item state

--- src/views/items/edit.tsx
+++ src/views/items/edit.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { MockItems } from 'mockdata';
+import { Item } from '@/views/items/list/WithItems';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 function EditItem() {
-  return <div>edit</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const [item, setItem] = React.useState<Item | null>(null);
+  const routeItem = state?.item;
+
+  React.useEffect(() => {
+    (() => {
+      if (routeItem) {
+        return setItem(routeItem);
+      }
+
+      if (id) {
+        const foundItem = MockItems.find((elm) => elm.id === parseInt(id, 10));
+        if (foundItem) {
+          return setItem(foundItem);
+        }
+      }
+
+      return navigate('/items');
+    })();
+  }, [id, navigate, routeItem]);
+
+  return <div>edit {item?.name}</div>;
 }
 
 export default EditItem;
