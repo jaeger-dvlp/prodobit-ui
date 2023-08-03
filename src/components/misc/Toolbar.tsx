27335c29c99f67fe046a02960c5a816169c9138c
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-03 16:05:58 +0300

    rev: toolbar anim

--- src/components/misc/Toolbar.tsx
+++ src/components/misc/Toolbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Box } from '@mantine/core';
+import { motion } from 'framer-motion';
 
 function ToolbarContainer({ children }: { children?: React.ReactNode }) {
   return (
     <Box
+      component={motion.aside}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.2,
+        duration: 0.5,
+        ease: 'easeInOut',
+      }}
       sx={{
         left: 0,
         bottom: 0,
