2d92a428e4649084efe469cbcf038bbae3289e33
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-03 15:41:21 +0300

    feat: right-col tables view

--- src/components/views/items/edit/Info.tsx
+++ src/components/views/items/edit/Info.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { motion } from 'framer-motion';
 import { ProdobitAppTheme as t } from '@/theme';
-import { Box, Button, Text } from '@mantine/core';
+import { Box, Button, Sx, Table, Text } from '@mantine/core';
 import ItemMainInfo from '@/components/views/items/edit/ItemMainInfo';
 import EditTextEditor from '@/components/views/items/edit/TextEditor';
 import ItemStatusBar from '@/components/views/items/edit/ItemStatusBar';
@@ -11,17 +12,21 @@ import {
   BarcodeIcon,
   Canlde2Icon,
   Category2Icon,
+  CopyIcon,
   CustomPlusIcon,
   DocIcon,
+  EditIcon,
   NotesIcon,
   RefreshIcon,
   RoutingIcon,
+  TrashIcon,
 } from '@/components/icons';
 
 const motionProps = {
   component: motion.section,
   animate: { opacity: 1, y: 0 },
   initial: { opacity: 0, y: 20 },
+  exit: { opacity: 0, y: 20 },
   transition: {
     damping: 30,
     type: 'spring',
@@ -29,30 +34,197 @@ const motionProps = {
   },
 };
 
+function ProductInfoTable() {
+  const MockValues = [
+    {
+      definition: 'Ürün Tam Adı',
+      value: 'Adidas AX2S Terrex Erkek Outdoor Ayakkabı',
+    },
+    {
+      definition: 'Seri Numarası',
+      value: 'C02',
+    },
+  ];
+
+  const RowButtonsSx: Sx = {
+    border: 'none',
+    height: 'auto',
+    borderRadius: 7,
+    padding: '5px',
+    backgroundColor: `transparent`,
+    transition: 'all 150ms ease-in-out',
+    ':hover': {
+      backgroundColor: t.colors.gray[3],
+    },
+  };
+
+  const Rows = MockValues.map((item, index) => (
+    <tr key={index}>
+      <td>{item.definition}</td>
+      <td>{item.value}</td>
+      <td>
+        <Button sx={RowButtonsSx} variant="default">
+          <EditIcon width={15} height={15} />
+        </Button>
+        <Button sx={RowButtonsSx} variant="default">
+          <CopyIcon width={15} height={15} />
+        </Button>
+        <Button sx={RowButtonsSx} variant="default">
+          <TrashIcon width={15} height={15} />
+        </Button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <Box
+      {...motionProps}
+      sx={{
+        padding: 30,
+        width: '100%',
+        borderRadius: 30,
+        border: '1px solid #DFDEE1',
+      }}
+    >
+      <Table
+        sx={{
+          margin: 0,
+          padding: 0,
+          th: {
+            fontWeight: 700,
+            fontsize: '12px',
+            lineHeight: '14.4px',
+            color: t.colors.gray[9],
+            padding: '0px!important',
+            paddingBottom: '10px!important',
+          },
+          'tbody tr': {
+            position: 'relative',
+            transition: 'all 150ms ease-in-out',
+            td: {
+              width: 'fit-content',
+              maxWidth: '200px',
+              fontsize: '12px',
+              lineHeight: '14.4px',
+              color: t.colors.gray[5],
+              padding: '10px 0px!important',
+              transition: 'all 150ms ease-in-out',
+              ':nth-child(1)': {
+                width: 'fit-content',
+                maxWidth: 200,
+              },
+              ':nth-child(2)': {
+                maxWidth: 150,
+                color: t.colors.gray[9],
+              },
+              ':nth-child(3)': {
+                gao: 10,
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'end',
+              },
+              button: {
+                color: t.colors.gray[5],
+              },
+            },
+            ':hover': {
+              transform: 'scale(1.02)',
+              td: {
+                color: t.colors.gray[9],
+                button: {
+                  color: t.colors.gray[9],
+                },
+              },
+            },
+          },
+        }}
+      >
+        <thead>
+          <tr>
+            <th>Özellik Tanımı</th>
+            <th>Özelliği</th>
+            <th
+              style={{
+                display: 'flex',
+                justifyContent: 'end',
+              }}
+            >
+              <Button
+                variant="default"
+                sx={{
+                  border: 'none',
+                  height: 'auto',
+                  borderRadius: 100,
+                  padding: '5px 10px',
+                  color: t.colors.gray[0],
+                  backgroundColor: `${t.colors.gray[8]}!important`,
+                }}
+              >
+                <CustomPlusIcon width={15} height={15} />
+              </Button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>{Rows}</tbody>
+      </Table>
+    </Box>
+  );
+}
+
+function ProductPlaceholderBox({ text }: { text: string }) {
+  return (
+    <Box
+      {...motionProps}
+      sx={{
+        padding: 30,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text
+        sx={{
+          borderRadius: 30,
+          fontSize: '12px',
+          padding: '10px 20px',
+          color: t.colors.gray[8],
+          backgroundColor: t.colors.gray[3],
+        }}
+      >
+        {text}
+      </Text>
+    </Box>
+  );
+}
+
 function InfoSpecBox() {
   const localSpecs = [
     {
       icon: BarcodeIcon,
       text: 'Öğe\nKimliği',
-      component: Box,
+      component: ProductInfoTable,
     },
     {
       icon: Category2Icon,
       text: 'Öğe\nÖzellikleri',
-      component: Box,
+      component: () => <ProductPlaceholderBox text="Özellikler henüz eklenmedi" />,
     },
     {
       icon: Canlde2Icon,
       text: 'Öğe\nSeçeneği',
-      component: Box,
+      component: () => <ProductPlaceholderBox text="Seçenekler henüz eklenmedi" />,
     },
     {
       icon: CustomPlusIcon,
       text: 'Yeni\nEkle',
-      component: Box,
+      component: () => <ProductPlaceholderBox text="Yeni özellik eklemek için tıklayın" />,
     },
   ];
   const [spec, setSpec] = React.useState(localSpecs[0]);
+
+  const LocalSpecComp = spec.component;
+
   return (
     <Box {...motionProps}>
       <Box
@@ -122,11 +294,22 @@ function InfoSpecBox() {
           </Box>
         ))}
       </Box>
+      <Box
+        sx={{
+          marginTop: 20,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'start',
+        }}
+      >
+        <LocalSpecComp />
+      </Box>
     </Box>
   );
 }
 
-function SupplierSpecBox() {
+function PlaceholderSpecBox({ text }: { text: string }) {
   return (
     <Box {...motionProps} sx={{ display: 'flex', justifyContent: 'center' }}>
       <Text
@@ -138,43 +321,7 @@ function SupplierSpecBox() {
           backgroundColor: t.colors.blue[1],
         }}
       >
-        Tedarikçiler
-      </Text>
-    </Box>
-  );
-}
-
-function ProgressSpecBox() {
-  return (
-    <Box {...motionProps} sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Text
-        sx={{
-          fontSize: '14px',
-          padding: '5px 20px',
-          borderRadius: 100,
-          color: t.colors.blue[7],
-          backgroundColor: t.colors.blue[1],
-        }}
-      >
-        İlerleyiş
-      </Text>
-    </Box>
-  );
-}
-
-function NotesSpecBox() {
-  return (
-    <Box {...motionProps} sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Text
-        sx={{
-          fontSize: '14px',
-          padding: '5px 20px',
-          borderRadius: 100,
-          color: t.colors.blue[7],
-          backgroundColor: t.colors.blue[1],
-        }}
-      >
-        Notlar
+        {text}
       </Text>
     </Box>
   );
@@ -189,17 +336,17 @@ const SpecButtons = [
   {
     icon: RefreshIcon,
     text: 'Tedarikçiler',
-    component: SupplierSpecBox,
+    component: () => <PlaceholderSpecBox text="Tedarikçiler henüz eklenmedi" />,
   },
   {
     icon: RoutingIcon,
     text: 'İlerleyiş',
-    component: ProgressSpecBox,
+    component: () => <PlaceholderSpecBox text="İlerleyiş henüz eklenmedi" />,
   },
   {
     icon: NotesIcon,
     text: 'Notlar',
-    component: NotesSpecBox,
+    component: () => <PlaceholderSpecBox text="Notlar henüz eklenmedi" />,
   },
 ];
 
