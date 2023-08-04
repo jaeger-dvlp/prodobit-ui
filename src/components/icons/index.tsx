ab47022d35c514b35ab628fa15e820ab8a872ede
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-04 14:56:11 +0300

    feat: export edit view icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1403,6 +1403,41 @@ function FolderIllustration(props: CustomIconProps) {
   );
 }
 
+function UploadDocumentIcon(props: CustomIconProps) {
+  return (
+    <svg viewBox="0 0 25 24" fill="none" {...DefaultProps} {...props}>
+      <path
+        d="M9.5 17V11L7.5 13"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.5 11L11.5 13"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22.5 10V15C22.5 20 20.5 22 15.5 22H9.5C4.5 22 2.5 20 2.5 15V9C2.5 4 4.5 2 9.5 2H14.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22.5 10H18.5C15.5 10 14.5 9 14.5 6V2L22.5 10Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
 export {
   DashboardIcon,
   AnalyzesIcon,
@@ -1462,4 +1497,5 @@ export {
   CopyIcon,
   Cube3dIcon,
   FolderIllustration,
+  UploadDocumentIcon,
 };
