0004a16dd2561dffde67715667ce996a6e08f909
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-10 10:09:27 +0300

    feat: new item step bar

--- src/views/items/new.tsx
+++ src/views/items/new.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Box } from '@mantine/core';
+import { Box, Text } from '@mantine/core';
 import { motion } from 'framer-motion';
 import { ProdobitAppTheme as t } from '@/theme';
 import NewItemWrapper from '@/components/context/NewItem.context';
+import { CustomCheckIcon } from '@/components/icons';
 
 function NewItemStepBar() {
   return (
@@ -10,11 +11,14 @@ function NewItemStepBar() {
       component="ul"
       sx={{
         gap: 0,
+        top: 0,
+        left: 0,
         margin: 0,
         padding: 0,
         width: '100%',
         display: 'flex',
         listStyle: 'none',
+        position: 'sticky',
         flexWrap: 'nowrap',
         flexDirection: 'row',
         backdropFilter: 'blur(32px)',
@@ -22,6 +26,7 @@ function NewItemStepBar() {
         borderBottom: `1px solid ${t.colors.gray[5]}`,
         [t.fn.smallerThan('md')]: {
           flexWrap: 'wrap',
+          position: 'relative',
         },
         '> li': {
           gap: 5,
@@ -38,13 +43,111 @@ function NewItemStepBar() {
               borderBottom: `1px solid ${t.colors.gray[5]}`,
             },
           },
+          '> .ct-icon-completed': {
+            width: 24,
+            height: 24,
+            color: t.colors.green[7],
+          },
+          '> .content': {
+            gap: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            '> .ct-h': {
+              fontWeight: 700,
+              fontSize: '12px',
+              lineHeight: '14.4px',
+            },
+            '> .ct-h.ct-h-completed': {
+              color: t.colors.green[7],
+            },
+            '> .ct-h.ct-h-waiting': {
+              color: t.colors.orange[4],
+            },
+            '> .ct-desc': {
+              fontWeight: 400,
+              fontSize: '15px',
+              lineHeight: '18px',
+              color: t.colors.gray[8],
+            },
+            '> .ct-s': {
+              gap: 10,
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              '> .ct-s-t': {
+                fontWeight: 700,
+                fontSize: '12px',
+                lineHeight: '14.4px',
+                color: t.colors.gray[6],
+              },
+              '> .ct-s-steps': {
+                gap: 3,
+                margin: 0,
+                padding: 0,
+                display: 'flex',
+                listStyle: 'none',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                '> li': {
+                  width: 8,
+                  height: 8,
+                  borderRadius: '50%',
+                  backgroundColor: 'transparent',
+                  border: `1px solid ${t.colors.gray[5]}`,
+                  ':nth-child(1), :nth-child(2)': {
+                    backgroundColor: t.colors.gray[8],
+                    border: `1px solid ${t.colors.gray[8]}`,
+                  },
+                  ':nth-child(3)': {
+                    backgroundColor: t.colors.green[6],
+                    border: `1px solid ${t.colors.green[6]}`,
+                  },
+                },
+              },
+            },
+          },
         },
       }}
     >
-      <Box component="li">-_-</Box>
-      <Box component="li">-_-</Box>
-      <Box component="li">-_-</Box>
-      <Box component="li">-_-</Box>
+      <Box component="li">
+        <Box className="content">
+          <Text className="ct-h ct-h-completed">Tamamlandı</Text>
+          <Text className="ct-desc">1. Varyasyon Seçimi</Text>
+        </Box>
+        <CustomCheckIcon className="ct-icon-completed" />
+      </Box>
+      <Box component="li">
+        <Box className="content">
+          <Text className="ct-h ct-h-completed">Tamamlandı</Text>
+          <Text className="ct-desc">2. Öğe Kimliği Belirleme</Text>
+        </Box>
+        <CustomCheckIcon className="ct-icon-completed" />
+      </Box>
+      <Box component="li">
+        <Box className="content">
+          <Box className="ct-s">
+            <Text className="ct-s-t">İşleniyor</Text>
+            <Box component="ul" className="ct-s-steps">
+              <Box component="li" />
+              <Box component="li" />
+              <Box component="li" />
+              <Box component="li" />
+              <Box component="li" />
+            </Box>
+          </Box>
+          <Text className="ct-desc">3. İşleme</Text>
+        </Box>
+      </Box>
+      <Box component="li">
+        <Box className="content">
+          <Text className="ct-h ct-h-waiting">Beklemede</Text>
+          <Text className="ct-desc">4. Öğe İsmi</Text>
+        </Box>
+      </Box>
     </Box>
   );
 }
