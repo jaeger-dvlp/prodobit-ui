33669c2ac24606fea346117b29cea57391003b29
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-10 11:55:07 +0300

    feat: export new item view icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1579,6 +1579,27 @@ function ArrowLineIcon(props: CustomIconProps) {
   );
 }
 
+function CustomXICon(props: CustomIconProps) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...DefaultProps} {...props}>
+      <path
+        d="M18 6L6 18"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6 6L18 18"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
 export {
   DashboardIcon,
   AnalyzesIcon,
@@ -1644,4 +1665,5 @@ export {
   ExternalUrlIcon,
   CustomCheckIcon,
   ArrowLineIcon,
+  CustomXICon,
 };
