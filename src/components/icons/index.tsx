e317128d9eccab492467dc2d81c10775517b0b03
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-15 10:39:45 +0300

    feat: export new item view icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1780,6 +1780,34 @@ function CustomSaveIcon(props: CustomIconProps) {
   );
 }
 
+function CustomSave2Icon(props: CustomIconProps) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...DefaultProps} {...props}>
+      <path
+        d="M19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H16L21 8V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17 21V13H7V21"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7 3V8H15"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
 function CustomProgrammingArrowIcon(props: CustomIconProps) {
   return (
     <svg viewBox="0 0 18 18" fill="none" {...DefaultProps} {...props}>
@@ -1903,5 +1931,6 @@ export {
   CustomEditPencilStIcon,
   CustomBackSquareIcon,
   CustomSaveIcon,
+  CustomSave2Icon,
   CustomProgrammingArrowIcon,
 };
