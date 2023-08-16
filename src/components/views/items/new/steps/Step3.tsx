e27622382d575a282c0a4881865fa2c7305b8833
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-16 15:44:33 +0300

    feat: template menu with sortable feature

--- src/components/views/items/new/steps/Step3.tsx
+++ src/components/views/items/new/steps/Step3.tsx
@@ -1,19 +1,43 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { motion } from 'framer-motion';
+import { CSS } from '@dnd-kit/utilities';
 import { ProdobitAppTheme as t } from '@/theme';
 import NewItemToolbar from '@/components/views/items/new/Toolbar';
 import { useNewItem } from '@/components/context/NewItem.context';
 import { stepContainerMotionProps } from '@/components/views/items/new/steps';
-import { Box, Button, Divider, Sx, Text, TextInput, Title, Tooltip } from '@mantine/core';
+import { Box, Button, Divider, Menu, Sx, Text, TextInput, Title, Tooltip } from '@mantine/core';
 
 import {
+  useSensor,
+  useSensors,
+  DndContext,
+  DragEndEvent,
+  PointerSensor,
+  closestCenter,
+  KeyboardSensor,
+} from '@dnd-kit/core';
+
+import {
+  arrayMove,
+  useSortable,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import {
+  TrashIcon,
   SearchIcon,
   CustomXICon,
+  CustomPlusIcon,
   CustomSaveIcon,
   CustomCheckIcon,
   CustomBackSquareIcon,
   CustomEditPencilStIcon,
   CustomRightLongChevronIcon,
+  CustomSmoothTooltipIllustration,
+  CustomSortThreeLine,
 } from '@/components/icons';
 
 const PropFieldListElmSX: Sx = {
@@ -38,6 +62,231 @@ const PropFieldListElmSX: Sx = {
     },
   },
 };
+
+function TemplateMenu({
+  state,
+  setState,
+}: {
+  state: MockTemplateSpec[];
+  setState: React.Dispatch<React.SetStateAction<MockTemplateSpec[]>>;
+}) {
+  function SortableItem({ id, children }: { id: number; children: React.ReactNode }) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+      id,
+    });
+
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+
+    return (
+      <Box component="li" ref={setNodeRef} style={style}>
+        <CustomSortThreeLine {...attributes} {...listeners} />
+        {children}
+      </Box>
+    );
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setState((old: any[]) => {
+        const oldIndex = old.findIndex((item) => item.id === active.id);
+        const newIndex = old.findIndex((item) => item.id === over?.id);
+
+        return arrayMove(old, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return (
+    <Menu.Dropdown
+      sx={{
+        gap: 10,
+        padding: 0,
+        marginLeft: -30,
+        width: 'auto',
+        maxWidth: 417,
+        border: 'none',
+        display: 'flex',
+        borderRadius: 20,
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        backgroundColor: 'transparent',
+      }}
+    >
+      <CustomSmoothTooltipIllustration
+        width={30}
+        height={100}
+        style={{
+          right: '0',
+          top: '50%',
+          zIndex: 9999,
+          color: '#fff',
+          position: 'absolute',
+          transform: 'translateY(-50%) translateX(100%) rotate(180deg)',
+        }}
+      />
+      <Box
+        sx={{
+          gap: 10,
+          width: '90vw',
+          maxWidth: 417,
+          display: 'flex',
+          borderRadius: 20,
+          padding: '30px 36px',
+          alignItems: 'stretch',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          justifyContent: 'flex-start',
+          boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+          '> button': {
+            width: '100%',
+            border: 'none',
+            height: 'auto',
+            padding: '20px 16px',
+            backgroundColor: 'transparent!important',
+            '> div > span': {
+              gap: 5,
+              color: '#000',
+              width: '100%',
+              display: 'flex',
+              fontWeight: 500,
+              fontSize: '15px',
+              lineHeight: '18px',
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+
+              '> svg': {
+                width: 24,
+                height: 24,
+              },
+            },
+          },
+        }}
+      >
+        <Box
+          component="ul"
+          sx={{
+            gap: 10,
+            margin: 0,
+            padding: 10,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'stretch',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            listStyle: 'none',
+            '> li': {
+              gap: 14,
+              padding: 20,
+              width: '100%',
+              display: 'flex',
+              borderRadius: 10,
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              backgroundColor: t.colors.gray[1],
+              '> svg': {
+                width: 24,
+                height: 24,
+                cursor: 'grab',
+                color: t.colors.gray[4],
+                outline: 'none',
+                ':active': {
+                  cursor: 'grabbing',
+                },
+              },
+              '.mantine-TextInput-input': {
+                padding: 0,
+                width: '100%',
+                border: 'none',
+                backgroundColor: 'transparent',
+              },
+              '> button': {
+                padding: 0,
+                color: '#000',
+                border: 'none',
+                height: 'auto',
+                backgroundColor: 'transparent!important',
+                '> div > span > svg': {
+                  width: 24,
+                  height: 24,
+                },
+              },
+            },
+          }}
+        >
+          <DndContext
+            sensors={sensors}
+            onDragEnd={handleDragEnd}
+            collisionDetection={closestCenter}
+          >
+            <SortableContext
+              items={state.map((item: any) => item.id)}
+              strategy={verticalListSortingStrategy}
+            >
+              {state.map((item) => (
+                <SortableItem id={item.id} key={`sr-st3-0-${item.id}`}>
+                  <TextInput defaultValue={item.name} />
+                  <Button variant="default">
+                    <TrashIcon />
+                  </Button>
+                </SortableItem>
+              ))}
+            </SortableContext>
+          </DndContext>
+        </Box>
+        <Button variant="default">
+          <Text>Yeni Ana Kategori</Text>
+          <CustomPlusIcon />
+        </Button>
+      </Box>
+      <Button
+        variant="default"
+        sx={{
+          marginTop: 5,
+          color: '#fff',
+          width: '100%',
+          height: 'auto',
+          border: 'none',
+          borderRadius: 20,
+          padding: '25px 30px',
+          backgroundColor: t.colors.green[6],
+          transition: 'all 0.15s ease-in-out',
+          boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+          '&:hover': {
+            backgroundColor: t.colors.green[5],
+          },
+          '> div > span': {
+            gap: 5,
+            width: '100%',
+            display: 'flex',
+            fontWeight: 500,
+            fontSize: '18px',
+            alignItems: 'center',
+            flexDirection: 'row',
+            color: t.colors.gray[0],
+            justifyContent: 'center',
+          },
+        }}
+      >
+        <Text>Kaydet</Text>
+      </Button>
+    </Menu.Dropdown>
+  );
+}
 
 function StepDescription() {
   return (
@@ -77,10 +326,27 @@ function StepDescription() {
     </Box>
   );
 }
-function ItemTemplateSelector() {
-  const [mockSpecs, setMockSpecs] = React.useState([
+
+type MockTemplateSpec = {
+  id: number;
+  name: string;
+  isSelected: boolean;
+  subSpecs: {
+    id: number;
+    name: string;
+  }[];
+};
+
+function ItemTemplateSelector({
+  focusMain,
+  blurMain,
+}: {
+  focusMain: () => void;
+  blurMain: () => void;
+}) {
+  const [mockSpecs, setMockSpecs] = React.useState<MockTemplateSpec[]>([
     {
-      id: 0,
+      id: 1,
       name: 'Fabrika Üretim',
       isSelected: false,
       subSpecs: [
@@ -91,7 +357,7 @@ function ItemTemplateSelector() {
       ],
     },
     {
-      id: 1,
+      id: 2,
       name: 'Perakende Satış',
       isSelected: true,
       subSpecs: [
@@ -114,7 +380,7 @@ function ItemTemplateSelector() {
       ],
     },
     {
-      id: 2,
+      id: 3,
       name: 'B2B',
       isSelected: false,
       subSpecs: [
@@ -207,7 +473,7 @@ function ItemTemplateSelector() {
                   fontsize: '15px',
                   lineHeight: '18px',
                   padding: '9px 0px',
-                  backgroundColor: 'transparent',
+                  backgroundColor: 'transparent!important',
                   transition: 'all 0.15s ease-in-out',
                   borderBottom: '1px solid transparent',
                   ':hover': {
@@ -237,24 +503,29 @@ function ItemTemplateSelector() {
               </Box>
             ))}
           </Box>
-          <Tooltip label="Düzenle">
-            <Button
-              sx={{
-                padding: 3,
-                height: 'auto',
-                border: 'none',
-                borderRadius: 5,
-                color: t.colors.gray[9],
-                backgroundColor: 'transparent',
-                transition: 'all 0.15s ease-in-out',
-                ':hover': {
-                  backgroundColor: t.colors.gray[3],
-                },
-              }}
-            >
-              <CustomEditPencilStIcon width={14} height={14} />
-            </Button>
-          </Tooltip>
+          <Menu onOpen={blurMain} onClose={focusMain} zIndex={999} position="left">
+            <Menu.Target>
+              <Tooltip label="Düzenle">
+                <Button
+                  sx={{
+                    padding: 3,
+                    height: 'auto',
+                    border: 'none',
+                    borderRadius: 5,
+                    color: t.colors.gray[9],
+                    backgroundColor: 'transparent',
+                    transition: 'all 0.15s ease-in-out',
+                    ':hover': {
+                      backgroundColor: t.colors.gray[3],
+                    },
+                  }}
+                >
+                  <CustomEditPencilStIcon width={14} height={14} />
+                </Button>
+              </Tooltip>
+            </Menu.Target>
+            <TemplateMenu state={mockSpecs} setState={setMockSpecs} />
+          </Menu>
         </Box>
 
         <Box
@@ -665,6 +936,9 @@ function ItemFieldSearchArea() {
 
 function NewItemStep3() {
   const { setCurrentStep } = useNewItem();
+  const [focusedMain, setFocusedMain] = React.useState(true);
+  const focusMain = () => setFocusedMain(true);
+  const blurMain = () => setFocusedMain(false);
   const goToNextStep = () => setCurrentStep((c) => c + 1);
   const goToPrevStep = () => setCurrentStep((c) => c - 1);
   return (
@@ -706,8 +980,8 @@ function NewItemStep3() {
             alignItems: 'center',
             flexDirection: 'column',
             justifyContent: 'center',
-            backgroundColor: 'rgba(255, 255, 255, 0.82)',
             boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+            backgroundColor: focusedMain ? '#fff' : 'rgba(255,255,255,0.5)',
           }}
         >
           <Box
@@ -722,7 +996,7 @@ function NewItemStep3() {
             }}
           >
             <StepDescription />
-            <ItemTemplateSelector />
+            <ItemTemplateSelector focusMain={focusMain} blurMain={blurMain} />
             <Divider w="100%" color={t.colors.gray[3]} />
             <ItemPropField />
             <ItemFieldSearchArea />
