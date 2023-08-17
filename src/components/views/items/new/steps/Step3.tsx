0cad2f446d8ca54d73e1bced6318817436faa246
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-17 13:17:52 +0300

    feat: save as template menu

--- src/components/views/items/new/steps/Step3.tsx
+++ src/components/views/items/new/steps/Step3.tsx
@@ -6,7 +6,18 @@ import { ProdobitAppTheme as t } from '@/theme';
 import NewItemToolbar from '@/components/views/items/new/Toolbar';
 import { useNewItem } from '@/components/context/NewItem.context';
 import { stepContainerMotionProps } from '@/components/views/items/new/steps';
-import { Box, Button, Divider, Menu, Sx, Text, TextInput, Title, Tooltip } from '@mantine/core';
+import {
+  Box,
+  Button,
+  Divider,
+  Menu,
+  Select,
+  Sx,
+  Text,
+  TextInput,
+  Title,
+  Tooltip,
+} from '@mantine/core';
 
 import {
   useSensor,
@@ -427,6 +438,160 @@ function TemplateFieldMenu() {
             </Box>
           ))}
         </Box>
+      </Box>
+      <Button
+        variant="default"
+        sx={{
+          marginTop: 5,
+          color: '#fff',
+          width: '100%',
+          height: 'auto',
+          border: 'none',
+          borderRadius: 20,
+          padding: '25px 30px',
+          backgroundColor: t.colors.green[6],
+          transition: 'all 0.15s ease-in-out',
+          boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+          '&:hover': {
+            backgroundColor: t.colors.green[5],
+          },
+          '> div > span': {
+            gap: 5,
+            width: '100%',
+            display: 'flex',
+            fontWeight: 500,
+            fontSize: '18px',
+            alignItems: 'center',
+            flexDirection: 'row',
+            color: t.colors.gray[0],
+            justifyContent: 'center',
+          },
+        }}
+      >
+        <Text>Kaydet</Text>
+      </Button>
+    </Menu.Dropdown>
+  );
+}
+
+function SaveTemplateMenu() {
+  return (
+    <Menu.Dropdown
+      sx={{
+        gap: 10,
+        padding: 0,
+        marginLeft: -40,
+        width: 'auto',
+        maxWidth: 417,
+        border: 'none',
+        display: 'flex',
+        borderRadius: 20,
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        backgroundColor: 'transparent',
+      }}
+    >
+      <CustomSmoothTooltipIllustration
+        width={30}
+        height={100}
+        style={{
+          right: '0',
+          top: '50%',
+          zIndex: 9999,
+          color: '#fff',
+          position: 'absolute',
+          transform: 'translateY(-50%) translateX(100%) rotate(180deg)',
+        }}
+      />
+      <Box
+        sx={{
+          gap: 25,
+          width: '90vw',
+          maxWidth: 417,
+          display: 'flex',
+          borderRadius: 20,
+          padding: '30px 36px',
+          alignItems: 'stretch',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          justifyContent: 'flex-start',
+          boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+          '> .mantine-TextInput-root': {
+            paddingBottom: 18,
+            borderBottom: `1px solid ${t.colors.gray[3]}`,
+            '.mantine-TextInput-input': {
+              padding: 0,
+              border: 'none',
+              width: '100%',
+              fontWeight: 500,
+              fontSize: '31px',
+              lineHeight: '37.2px',
+              color: t.colors.gray[9],
+              backgroundColor: 'transparent!important',
+            },
+          },
+        }}
+      >
+        <Text
+          sx={{
+            fontWeight: 500,
+            fontSize: '15px',
+            lineHeight: '18px',
+            color: t.colors.gray[6],
+          }}
+        >
+          Şablon Adı
+        </Text>
+        <TextInput defaultValue="Kargo" />
+        <Text
+          sx={{
+            fontWeight: 500,
+            fontSize: '15px',
+            lineHeight: '18px',
+            color: t.colors.gray[6],
+          }}
+        >
+          Ana Kategori Belirleyin
+        </Text>
+        <Select
+          styles={{
+            input: {
+              padding: 20,
+              border: 'none',
+              borderRadius: 10,
+              fontSize: '15px',
+              fontWeight: 400,
+              lineHeight: '18px',
+              color: '#000',
+              minHeight: 58,
+              '::placeholder': {
+                color: '#000',
+              },
+              backgroundColor: t.colors.gray[1],
+            },
+            dropdown: {
+              padding: 10,
+              borderRadius: 15,
+              boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+            },
+            item: {
+              borderRadius: 10,
+              "&[data-selected='true']": {
+                backgroundColor: `${t.colors.green[6]}!important`,
+              },
+            },
+          }}
+          placeholder="Seçin"
+          defaultValue="perakende-satis"
+          data={[
+            { label: 'Perakende Satış', value: 'perakende-satis' },
+            {
+              label: 'Deneme',
+              value: 'deneme',
+            },
+          ]}
+        />
       </Box>
       <Button
         variant="default"
@@ -995,30 +1160,35 @@ function ItemPropField({ focusMain, blurMain }: { focusMain: () => void; blurMai
           >
             <CustomBackSquareIcon />
           </Button>
-          <Button
-            variant="default"
-            sx={{
-              padding: 0,
-              height: 'auto',
-              border: 'none',
-              color: t.colors.gray[9],
-              backgroundColor: 'transparent!important',
-              '> div > span': {
-                gap: 7,
-                display: 'flex',
-                alignItems: 'center',
-                flexDirection: 'row',
-                justifyContent: 'flex-end',
-                '> svg': {
-                  width: 18,
-                  height: 18,
-                },
-              },
-            }}
-          >
-            <CustomSaveIcon />
-            <Text>Şablon Olarak Kaydet</Text>
-          </Button>
+          <Menu onOpen={blurMain} onClose={focusMain} zIndex={999} position="right">
+            <Menu.Target>
+              <Button
+                sx={{
+                  padding: 0,
+                  height: 'auto',
+                  border: 'none',
+                  color: t.colors.gray[9],
+                  backgroundColor: 'transparent!important',
+                  '> div > span': {
+                    gap: 7,
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    '> svg': {
+                      width: 18,
+                      height: 18,
+                    },
+                  },
+                }}
+                variant="default"
+              >
+                <CustomSaveIcon />
+                <Text>Şablon Olarak Kaydet</Text>
+              </Button>
+            </Menu.Target>
+            <SaveTemplateMenu />
+          </Menu>
         </Box>
         <Box
           component="ul"
