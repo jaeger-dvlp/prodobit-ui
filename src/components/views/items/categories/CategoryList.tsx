38e5806088e06e29f908416dc35ba18344574b22
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-21 09:44:32 +0300

    feat: ct-list-elm menu

--- src/components/views/items/categories/CategoryList.tsx
+++ src/components/views/items/categories/CategoryList.tsx
@@ -1,16 +1,110 @@
 import React from 'react';
 import { ProdobitAppTheme as t } from '@/theme';
-import { Box, Button, Text } from '@mantine/core';
+import { Box, Button, Menu, Text, TextInput } from '@mantine/core';
 import {
   CustomHorizontal3DotsIcon,
+  CustomMoveIcon,
   CustomPlusIcon,
+  CustomSmoothTooltipIllustration,
   CustomVertical3DotsIcon,
+  TrashIcon,
 } from '@/components/icons';
 import { ItemCategory } from '@/views/items/list/WithItems';
 
 type Props = {
   categories: ItemCategory[];
 };
+
+function CtMenu({ defaultValue }: { defaultValue?: string }) {
+  return (
+    <Menu.Dropdown
+      sx={{
+        padding: 0,
+        marginLeft: 90,
+        backgroundColor: 'transparent',
+        '> div > .ct-menu-tooltip': {
+          left: 0,
+          top: '50%',
+          width: 30,
+          height: 100,
+          zIndex: 9999,
+          color: 'white',
+          position: 'absolute',
+          transform: 'translateX(-100%) translateY(-50%)',
+        },
+        '> div > .ct-menu-wrapper': {
+          gap: 25,
+          width: '90vw',
+          maxWidth: 418,
+          display: 'flex',
+          borderRadius: 20,
+          padding: '35px 30px',
+          alignItems: 'stretch',
+          flexDirection: 'column',
+          backgroundColor: '#fff',
+          justifyContent: 'flex-start',
+          boxShadow: '0px 37px 44px -13px rgba(104, 48, 48, 0.10)',
+          '> .ct-name-label': {
+            fontSize: '15px',
+            fontWeight: 500,
+            lineHeight: '18px',
+            color: t.colors.gray[6],
+          },
+          '> .ct-name-input .mantine-TextInput-input': {
+            height: 'auto',
+            padding: 0,
+            border: 'none',
+            borderRadius: 0,
+            fontWeight: 500,
+            fontSize: '31px',
+            paddingBottom: 18,
+            backgroundColor: 'transparent',
+            borderBottom: `1px solid ${t.colors.gray[3]}`,
+          },
+          '> button': {
+            border: 'none',
+            height: 'auto',
+            padding: '20px',
+            borderRadius: 10,
+            backgroundColor: t.colors.gray[0],
+            '&:hover': {
+              backgroundColor: t.colors.gray[1],
+            },
+            '> div > span': {
+              gap: 2,
+              color: '#000',
+              width: '100%',
+              display: 'flex',
+              fontWeight: 400,
+              fontSize: '15px',
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              '> svg': {
+                width: 24,
+                height: 24,
+              },
+            },
+          },
+        },
+      }}
+    >
+      <CustomSmoothTooltipIllustration className="ct-menu-tooltip" />
+      <Box className="ct-menu-wrapper">
+        <Text className="ct-name-label">Kategori Adı</Text>
+        <TextInput className="ct-name-input" defaultValue={defaultValue} />
+        <Button variant="default">
+          <Text>Sil</Text>
+          <TrashIcon />
+        </Button>
+        <Button variant="default">
+          <Text>Taşı</Text>
+          <CustomMoveIcon />
+        </Button>
+      </Box>
+    </Menu.Dropdown>
+  );
+}
 
 function CategoryList({ categories }: Props) {
   return (
@@ -179,9 +273,14 @@ function CategoryList({ categories }: Props) {
             </Button>
             <Box className="ct-item-name-wrapper">
               <Text>{category.name}</Text>
-              <Button variant="default">
-                <CustomHorizontal3DotsIcon />
-              </Button>
+              <Menu zIndex={9999} position="right">
+                <Menu.Target>
+                  <Button variant="default">
+                    <CustomHorizontal3DotsIcon />
+                  </Button>
+                </Menu.Target>
+                <CtMenu defaultValue={category.name} />
+              </Menu>
             </Box>
             <Box className="ct-item-metadata">
               <Box className="sub-folds">
