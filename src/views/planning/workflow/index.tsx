6650bba8d8f7e041bf0f4dcd496dcea37d1686a1
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-21 11:00:27 +0300

    feat: navbar childs

--- src/views/planning/workflow/index.tsx
+++ src/views/planning/workflow/index.tsx
@@ -1,10 +1,75 @@
 import React from 'react';
-import { Box } from '@mantine/core';
 import { motion } from 'framer-motion';
+import { ProdobitAppTheme as t } from '@/theme';
 import Navbar from '@/components/layout/Navbar';
 import RoutesMap, { RouteMapItem } from '@/routes';
+import { Box, Button, Sx, Text } from '@mantine/core';
+import { BoldCalendarIcon, CustomRadarIcon } from '@/components/icons';
+
+type Views = 'calendar' | 'workflow';
+
+function ViewSelector({
+  view,
+  setView,
+}: {
+  view: Views;
+  setView: React.Dispatch<React.SetStateAction<Views>>;
+}) {
+  const getViewSx = (viewName: string): Sx => ({
+    gap: '10px',
+    height: 'auto',
+    border: 'none',
+    display: 'flex',
+    fontWeight: 500,
+    fontSize: '15px',
+    borderRadius: 100,
+    lineHeight: '18px',
+    padding: '9px 18px',
+    flexDirection: 'row',
+    alignContent: 'center',
+    color: t.colors.gray[8],
+    justifyContent: 'center',
+    transition: 'all 200ms ease-in-out',
+    opacity: view === viewName ? 1 : 0.6,
+    backgroundColor: view === viewName ? t.colors.gray[3] : 'transparent',
+    ':hover': {
+      backgroundColor: view === viewName ? t.colors.gray[3] : t.colors.gray[2],
+    },
+  });
+
+  return (
+    <Box
+      sx={{
+        gap: 1,
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        minWidth: 'fit-content',
+        justifyContent: 'center',
+      }}
+    >
+      <Button
+        variant="default"
+        onClick={() => setView('calendar')}
+        sx={(() => getViewSx('calendar'))()}
+      >
+        <BoldCalendarIcon width={16} height={16} />
+        <Text ml={10}>Takvim Görünümü</Text>
+      </Button>
+      <Button
+        variant="default"
+        onClick={() => setView('workflow')}
+        sx={(() => getViewSx('workflow'))()}
+      >
+        <CustomRadarIcon width={16} height={16} />
+        <Text ml={10}>Akış Görünümü</Text>
+      </Button>
+    </Box>
+  );
+}
 
 function PlanningWorkflow() {
+  const [view, setView] = React.useState<Views>('workflow');
   const Route = RoutesMap.find((route: RouteMapItem) => route.path === '/planning');
   const Route2 = Route?.subRoutes?.find(
     (route: RouteMapItem) => route.path === '/planning/workflow',
@@ -31,6 +96,7 @@ function PlanningWorkflow() {
           path: route?.path,
           name: route?.name || '?',
         }))}
+        middleChilds={<ViewSelector view={view} setView={setView} />}
       />
     </Box>
   );
