e8d602c47e192d2ba3ac1d5e4254e4b3845089ae
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-21 14:35:28 +0300

    feat: month selecgtor

--- src/views/planning/workflow/index.tsx
+++ src/views/planning/workflow/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import { motion } from 'framer-motion';
 import { ProdobitAppTheme as t } from '@/theme';
 import Navbar from '@/components/layout/Navbar';
 import RoutesMap, { RouteMapItem } from '@/routes';
-import { Box, Button, Sx, Text } from '@mantine/core';
+import { Box, Button, Select, Sx, Text } from '@mantine/core';
 import { BoldCalendarIcon, CustomRadarIcon } from '@/components/icons';
+
+import 'dayjs/locale/tr';
+
+dayjs.locale('tr');
 
 type Views = 'calendar' | 'workflow';
 
@@ -68,6 +73,78 @@ function ViewSelector({
   );
 }
 
+/* Control Bar | pkg's: dayjs
+Must list the days as a button in a horizontal row. When click to a day, it must be scroll to that day in the calendar & control bar.  (Must be scrollable to right & left)
+After that, must be list the next days to.
+When click to prev day, must be scroll to left. After that, must be list the prev days to.
+7 days must be listed in the control bar. ( Days per view is 7 )
+*/
+
+function ControlBar() {
+  const [currentMonth, setCM] = React.useState(dayjs().format('M'));
+  return (
+    <Box
+      sx={{
+        gap: 40,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        '> .month-selector': {
+          gap: 15,
+          maxWidth: 150,
+          display: 'flex',
+          alignItems: 'stretch',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          '> .mantine-Text-root': {
+            fontSize: '15px',
+            fontWeight: 500,
+            lineHeight: '18px',
+            color: t.colors.gray[9],
+          },
+          '.mantine-Select-input': {
+            fontSize: '15px',
+            fontWeight: 400,
+            borderRadius: 10,
+            lineHeight: '18px',
+            padding: '10px 15px 10px 10px',
+            backgroundColor: 'transparent',
+            border: '1px solid rgba(0, 0, 0, 0.20)',
+          },
+          '.mantine-Select-dropdown': {
+            borderRadius: 10,
+            '.mantine-Select-item': {
+              borderRadius: 10,
+              "&[data-selected='true']": {
+                backgroundColor: `${t.colors.green[5]}!important`,
+              },
+            },
+          },
+        },
+      }}
+      className="control-bar"
+    >
+      <Box className="month-selector">
+        <Text>Ay Seçimi</Text>
+        <Select
+          defaultValue={currentMonth}
+          onChange={(e: string) => {
+            setCM(e.toString());
+          }}
+          data={Array(12)
+            .fill(0)
+            .map((_, i) => ({
+              value: (i + 1).toString(),
+              label: dayjs().month(i).format('MMMM'),
+            }))}
+        />
+      </Box>
+    </Box>
+  );
+}
+
 function PlanningWorkflow() {
   const [view, setView] = React.useState<Views>('workflow');
   const Route = RoutesMap.find((route: RouteMapItem) => route.path === '/planning');
@@ -81,9 +158,14 @@ function PlanningWorkflow() {
       animate={{ opacity: 1 }}
       component={motion.section}
       sx={(theme) => ({
-        width: '100%',
-        minHeight: '100%',
+        gap: 50,
         padding: 60,
+        width: '100%',
+        display: 'flex',
+        minHeight: '100%',
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         backgroundColor: 'transparent',
         [theme.fn.smallerThan('md')]: {
           padding: 45,
@@ -98,6 +180,19 @@ function PlanningWorkflow() {
         }))}
         middleChilds={<ViewSelector view={view} setView={setView} />}
       />
+      <Box
+        sx={{
+          gap: 30,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'stretch',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <ControlBar />
+      </Box>
     </Box>
   );
 }
