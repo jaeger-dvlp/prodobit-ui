7a3a4f9d32ed6f56fd5067e38edbd7206c41ddd2
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-22 14:32:30 +0300

    feat: workflow wrapper

---
+++ src/components/context/Workflow.context.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import dayjs from 'dayjs';
+
+export type IWorkflow = {
+  currentDay: string;
+  currentMonth: string;
+  setCurrentDay: React.Dispatch<React.SetStateAction<string>>;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const WorkflowContext = React.createContext<IWorkflow>({
+  currentDay: dayjs().format('D'),
+  currentMonth: dayjs().format('M'),
+  setCurrentDay: () => {},
+  setCurrentMonth: () => {},
+});
+
+export default function WorkflowWrapper({ children }: { children: React.ReactNode }) {
+  const [currentDay, setCurrentDay] = React.useState<string>(dayjs().format('D'));
+  const [currentMonth, setCurrentMonth] = React.useState<string>(dayjs().format('M'));
+
+  const value = React.useMemo(
+    () => ({
+      currentDay,
+      currentMonth,
+      setCurrentDay,
+      setCurrentMonth,
+    }),
+    [currentDay, currentMonth],
+  );
+
+  return <WorkflowContext.Provider value={value}>{children}</WorkflowContext.Provider>;
+}
+
+export function useWorkflow() {
+  const context = React.useContext(WorkflowContext);
+  if (context === undefined) {
+    throw new Error('useWorkflow must be used within a WorkflowWrapper');
+  }
+  return context;
+}
