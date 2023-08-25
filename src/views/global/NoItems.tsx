f37dfc14c03c5b719b1e98bcc5c13795bfd5784e
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-25 11:28:13 +0300

    feat: make view generic

---
+++ src/views/global/NoItems.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Navbar from '@/components/layout/Navbar';
+import { Box, Image, Text } from '@mantine/core';
+import AddButtons from '@/components/views/itemslist/AddButtons';
+import ItemCountDisplay from '@/components/views/items/ItemCountDisplay';
+
+type NoOrdersViewProps<T extends Record<string, unknown>> = {
+  items: T[];
+  text: string;
+  primaryButtonText: string;
+  secondaryButtonText: string;
+  onPrimaryButtonClick: () => void;
+  onSecondaryButtonClick: () => void;
+  paths: { path?: string; name: string }[];
+};
+
+function GenericNoItemsView<T extends Record<string, unknown>>({
+  items,
+  paths,
+  text,
+  primaryButtonText,
+  secondaryButtonText,
+  onPrimaryButtonClick,
+  onSecondaryButtonClick,
+}: NoOrdersViewProps<T>) {
+  return (
+    <Box
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      component={motion.section}
+      sx={{
+        gap: 40,
+        padding: 30,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Navbar
+        sx={{ padding: 30 }}
+        withButtons
+        paths={paths}
+        middleChilds={<ItemCountDisplay itemCount={items.length} />}
+      />
+
+      <Image
+        fit="contain"
+        src="/assets/img/no-item.svg"
+        sx={{
+          width: '100%',
+          maxWidth: 700,
+        }}
+      />
+      <Text
+        maw={450}
+        sx={(theme) => ({
+          fontWeight: 300,
+          lineHeight: 1.3,
+          fontSize: '45px',
+          textAlign: 'center',
+          color: theme.colors.green[7],
+        })}
+      >
+        {text}
+      </Text>
+      <Box
+        sx={{
+          gap: 10,
+          marginTop: 75,
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <AddButtons
+          primaryButtonText={primaryButtonText}
+          secondaryButtonText={secondaryButtonText}
+          onPrimaryButtonClick={onPrimaryButtonClick}
+          onSecondaryButtonClick={onSecondaryButtonClick}
+        />
+      </Box>
+    </Box>
+  );
+}
+export default GenericNoItemsView;
