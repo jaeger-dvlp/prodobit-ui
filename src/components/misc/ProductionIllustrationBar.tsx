4401c6f94905bb5b7bfbcb100a7040830c4866e7
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-30 13:44:16 +0300

    feat: top & bottom bar of comp

--- src/components/misc/ProductionIllustrationBar.tsx
+++ src/components/misc/ProductionIllustrationBar.tsx
@@ -25,18 +25,129 @@ const styles = createStyles({
     gridTemplateColumns: 'repeat(12, 1fr)',
   },
   centerColumn: {
+    gap: 60,
+    padding: 30,
     width: '100%',
     height: '100%',
+    display: 'flex',
     borderRadius: 33,
-    padding: '30px',
     minHeight: '413px',
     gridColumn: 'span 6',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     backgroundColor: t.colors.gray[0],
     backdropFilter: 'blur(74.56938171386719px)',
     boxShadow:
       '0px 18.261890411376953px 22.827362060546875px 0px rgba(0, 0, 0, 0.05), -0.7609121203422546px 0.7609121203422546px 0.7609121203422546px -1.5218242406845093px rgba(255, 255, 255, 0.35) inset, 0px 0.7609121203422546px 6.087296962738037px 0px rgba(255, 255, 255, 0.35) inset',
     [t.fn.smallerThan('lg')]: {
       gridColumn: 'span 12',
+    },
+  },
+  ccMetaTopBar: {
+    gap: 10,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    '> .meta-texts': {
+      gap: 15,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      '> .indicator-group': {
+        gap: 6,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        '> .indicator-color': {
+          width: 12,
+          height: 12,
+          borderRadius: 3,
+          backgroundColor: t.colors.purple[7],
+        },
+        '> .indicator-text': {
+          fontSize: '12px',
+          fontWeight: 400,
+          lineHeight: '14.4px',
+          color: t.colors.gray[9],
+        },
+      },
+      '> .meta-counts': {
+        gap: 5,
+        display: 'flex',
+        alignItems: 'flex-end',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        '> .meta-count': {
+          fontSize: '31px',
+          fontWeight: 700,
+          lineHeight: 0.9,
+          color: t.colors.gray[9],
+        },
+        '> .meta-count-small': {
+          fontSize: '12px',
+          fontWeight: 400,
+          lineHeight: '14.4px',
+          color: t.colors.gray[9],
+        },
+      },
+    },
+    '> .view-size-cont': {
+      gap: 5,
+      padding: 5,
+      display: 'flex',
+      borderRadius: 15,
+      width: 'fit-content',
+      alignItems: 'stretch',
+      flexDirection: 'row',
+      backgroundColor: t.colors.gray[2],
+      '> .view-size-btn': {
+        height: 'auto',
+        border: 'none',
+        borderRadius: 10,
+        fontSize: '12px',
+        fontWeight: 400,
+        padding: '10px 15px',
+        color: t.colors.gray[7],
+        backgroundColor: 'transparent',
+        transition: 'all 0.15s ease-in-out',
+        '&:hover': {
+          backgroundColor: t.colors.gray[3],
+        },
+        "&[data-active='true']": {
+          color: t.colors.gray[0],
+          backgroundColor: t.colors.gray[8],
+        },
+      },
+    },
+  },
+  ccWeekSizeCont: {
+    gap: 0,
+    padding: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    justifyContent: 'stretch',
+    backgroundColor: 'transparent',
+    border: `1px solid ${t.colors.gray[2]}`,
+    '> .week-size-btn': {
+      flex: 1,
+      height: 'auto',
+      border: 'none',
+      fontWeight: 400,
+      fontSize: '12px',
+      borderRadius: 10,
+      padding: '10px 15px',
+      color: t.colors.gray[7],
+      backgroundColor: 'transparent',
+      transition: 'all 0.15s ease-in-out',
+      "&[data-active='true']": {
+        backgroundColor: t.colors.gray[2],
+      },
     },
   },
   cornerColumn: {
@@ -234,8 +345,65 @@ function LCornerButton({ children, color }: { children: React.ReactNode; color: 
   );
 }
 
+export type Size = {
+  label: string;
+  value: string;
+};
+
+const viewSizes: Size[] = [
+  {
+    label: 'Haftalık',
+    value: 'weekly',
+  },
+  {
+    label: 'Aylık',
+    value: 'monthly',
+  },
+  {
+    label: 'Yıllık',
+    value: 'yearly',
+  },
+];
+
+const weekSizes: Size[] = [
+  {
+    label: 'Tüm Hafta',
+    value: 'all',
+  },
+  {
+    label: 'PTS',
+    value: 'monday',
+  },
+  {
+    label: 'SAL',
+    value: 'tuesday',
+  },
+  {
+    label: 'ÇAR',
+    value: 'wednesday',
+  },
+  {
+    label: 'PER',
+    value: 'thursday',
+  },
+  {
+    label: 'CUM',
+    value: 'friday',
+  },
+  {
+    label: 'CMT',
+    value: 'saturday',
+  },
+  {
+    label: 'PAZ',
+    value: 'sunday',
+  },
+];
+
 function ProductionIllustrationBar() {
   const { classes } = styles();
+  const [viewSize, setViewSize] = React.useState<Size>(viewSizes[0]);
+  const [weekSize, setWeekSize] = React.useState<Size>(weekSizes[0]);
 
   return (
     <Box className={classes.container}>
@@ -260,7 +428,44 @@ function ProductionIllustrationBar() {
         </Box>
       </Box>
       <Box className={classes.centerColumn}>
-        Çözemezsin, yok. Bu düğüm kör düğüm. Korkmuyorum gecelerden, beni sen öldür.
+        <Box className={classes.ccMetaTopBar}>
+          <Box className="meta-texts">
+            <Box className="indicator-group">
+              <Box className="indicator-color" />
+              <Text className="indicator-text">Toplam Üretim</Text>
+            </Box>
+            <Box className="meta-counts">
+              <Text className="meta-count">15.000</Text>
+              <Text className="meta-count-small">ADET</Text>
+            </Box>
+          </Box>
+          <Box className="view-size-cont">
+            {viewSizes.map((size, i) => (
+              <Button
+                variant="default"
+                key={`view-size-${i}`}
+                className="view-size-btn"
+                data-active={viewSize === size}
+                onClick={() => setViewSize(size)}
+              >
+                {size.label}
+              </Button>
+            ))}
+          </Box>
+        </Box>
+        <Box className={classes.ccWeekSizeCont}>
+          {weekSizes.map((size, i) => (
+            <Button
+              variant="default"
+              key={`week-size-${i}`}
+              className="week-size-btn"
+              data-active={weekSize === size}
+              onClick={() => setWeekSize(size)}
+            >
+              {size.label}
+            </Button>
+          ))}
+        </Box>
       </Box>
       <Box className={classes.cornerColumn}>
         <Box className={classes.cornerIdentify}>
