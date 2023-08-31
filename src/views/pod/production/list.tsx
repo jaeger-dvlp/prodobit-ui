cc2c167df295eb79388fb489987e097ebb3717e8
JAEGER_ <omer.kayalar0101@icloud.com>
2023-08-31 15:58:54 +0300

    feat: seperate comps && goddamn hover thing

--- src/views/pod/production/list.tsx
+++ src/views/pod/production/list.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import { PodMockProdLine } from 'mockdata';
 import { ProdobitAppTheme as t } from '@/theme';
 import PodSidebar from '@/components/pod/sidebar';
-import { Box, Button, Text, Title, createStyles } from '@mantine/core';
-import { getRandomUUID } from '@/common/utils/misc';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Box, Button, Text, Title, createStyles } from '@mantine/core';
 
-const getProdItemPercent = (self: number, total: number) => {
+export const getProdItemPercent = (self: number, total: number) => {
   const percent = (self / total) * 100;
   return percent;
 };
 
-const getColorByStatus = (status: string, isMachine: boolean = false) => {
+export const getColorByStatus = (status: string, isMachine: boolean = false) => {
   switch (status) {
     case 'completed':
       if (isMachine) return t.colors.purple[5];
@@ -27,129 +27,6 @@ const getColorByStatus = (status: string, isMachine: boolean = false) => {
       return t.colors.gray[3];
   }
 };
-
-const data = [
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'completed',
-    production: [
-      {
-        count: 2000,
-        status: 'waiting',
-      },
-      {
-        count: 6000,
-        status: 'completed',
-      },
-      {
-        count: 3000,
-        status: 'stopped',
-      },
-      {
-        count: 7000,
-        status: 'ready',
-      },
-    ],
-  },
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'ready',
-    production: [
-      {
-        count: 7000,
-        status: 'completed',
-      },
-      {
-        count: 2000,
-        status: 'unknown',
-      },
-    ],
-  },
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'stopped',
-    production: [
-      {
-        count: 3000,
-        status: 'completed',
-      },
-      {
-        count: 6000,
-        status: 'ready',
-      },
-      {
-        count: 1000,
-        status: 'waiting',
-      },
-    ],
-  },
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'stopped',
-    production: [
-      {
-        count: 3000,
-        status: 'completed',
-      },
-      {
-        count: 6000,
-        status: 'ready',
-      },
-      {
-        count: 1000,
-        status: 'waiting',
-      },
-    ],
-  },
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'stopped',
-    production: [
-      {
-        count: 3000,
-        status: 'completed',
-      },
-      {
-        count: 6000,
-        status: 'ready',
-      },
-      {
-        count: 1000,
-        status: 'waiting',
-      },
-    ],
-  },
-  {
-    id: getRandomUUID(),
-    no: 'CN6',
-    name: 'M20X1.5 EMC BURÇ EURO',
-    status: 'stopped',
-    production: [
-      {
-        count: 9000,
-        status: 'completed',
-      },
-      {
-        count: 900,
-        status: 'ready',
-      },
-      {
-        count: 1000,
-        status: 'waiting',
-      },
-    ],
-  },
-];
 
 const Styles = createStyles({
   controlBar: {
@@ -212,112 +89,202 @@ const Styles = createStyles({
     justifyContent: 'flex-start',
   },
   prodLine: {
-    gap: 55,
+    gap: 0,
+    padding: 0,
     display: 'grid',
     borderRadius: 20,
-    padding: '20px 30px',
-    placeItems: 'center',
+    position: 'relative',
+    placeItems: 'stretch',
     placeContent: 'center',
     backdropFilter: 'blur(12.5px)',
     transition: 'all 0.2s ease-in-out',
     backgroundColor: 'rgba(255, 255, 255, 0.55)',
-    gridTemplateColumns: 'repeat(11, minmax(0, 1fr))',
+    gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
     '&:hover': {
       transform: 'scale(1.025)',
+      backgroundColor: 'rgba(255, 255, 255, 0.85)',
     },
-    [t.fn.smallerThan('lg')]: {
-      gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
-    },
-    '> .left-col': {
-      gap: 30,
-      width: '100%',
+    '> .main-panel': {
+      gap: 55,
       display: 'grid',
+      padding: '20px 30px',
       placeItems: 'center',
-      gridColumn: 'span 3',
-      placeContent: 'start',
-      gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+      placeContent: 'center',
+      gridTemplateColumns: 'repeat(11, minmax(0, 1fr))',
       [t.fn.smallerThan('lg')]: {
-        gridColumn: 'span 12',
+        gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
       },
-      '> .line-no': {
+      '> .left-col': {
+        gap: 30,
         width: '100%',
-        fontWeight: 500,
-        fontSize: '22px',
-        borderRadius: 100,
-        textAlign: 'center',
-        padding: '15px 30px',
-        color: t.colors.purple[0],
+        display: 'grid',
+        placeItems: 'center',
+        gridColumn: 'span 3',
+        placeContent: 'start',
+        gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+        [t.fn.smallerThan('lg')]: {
+          gridColumn: 'span 11',
+        },
+        '> .line-no': {
+          width: '100%',
+          fontWeight: 500,
+          fontSize: '22px',
+          borderRadius: 100,
+          textAlign: 'center',
+          padding: '15px 30px',
+          color: t.colors.purple[0],
+        },
+        '> .line-name': {
+          fontSize: '15px',
+          fontWeight: 700,
+          whiteSpace: 'pre-wrap',
+          color: t.colors.gray[9],
+        },
       },
-      '> .line-name': {
-        fontSize: '15px',
-        fontWeight: 700,
-        whiteSpace: 'pre-wrap',
-        color: t.colors.gray[9],
+      '> .center-col': {
+        width: '100%',
+        display: 'grid',
+        placeItems: 'center',
+        gridColumn: 'span 6',
+        placeContent: 'stretch',
+        gridTemplateColumns: 'repeat(4, minmax(0, 1fr))',
+        [t.fn.smallerThan('lg')]: {
+          gridColumn: 'span 11',
+        },
+        '> .cc-item': {
+          gap: 0,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'flex-start',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          '> .cc-title': {
+            opacity: 0.5,
+            fontSize: '12px',
+            fontWeight: 700,
+            lineHeight: 1.1,
+            color: t.colors.gray[9],
+          },
+          '> .cc-value': {
+            fontWeight: 500,
+            lineHeight: 1.1,
+            fontSize: '22px',
+            color: t.colors.gray[9],
+          },
+        },
+        '> .cc-desc': {
+          width: '100%',
+          maxWidth: 120,
+          fontSize: '15px',
+          fontWeight: 400,
+          textAlign: 'left',
+          whiteSpace: 'pre-wrap',
+          color: t.colors.gray[9],
+        },
       },
-    },
-    '> .center-col': {
-      width: '100%',
-      display: 'grid',
-      placeItems: 'center',
-      gridColumn: 'span 6',
-      placeContent: 'stretch',
-      gridTemplateColumns: 'repeat(4, minmax(0, 1fr))',
-      [t.fn.smallerThan('lg')]: {
-        gridColumn: 'span 12',
-      },
-      '> .cc-item': {
-        gap: 0,
+      '> .right-col': {
+        gap: 2,
         width: '100%',
         display: 'flex',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        '> .cc-title': {
-          opacity: 0.5,
-          fontSize: '12px',
-          fontWeight: 700,
-          lineHeight: 1.1,
-          color: t.colors.gray[9],
+        gridColumn: 'span 2',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        [t.fn.smallerThan('lg')]: {
+          gridColumn: 'span 11',
         },
-        '> .cc-value': {
-          fontWeight: 500,
-          lineHeight: 1.1,
-          fontSize: '22px',
-          color: t.colors.gray[9],
+        '> .prod-progress-item': {
+          height: 34,
+          width: '100%',
+          borderRadius: 5,
         },
-      },
-      '> .cc-desc': {
-        width: '100%',
-        maxWidth: 120,
-        fontSize: '15px',
-        fontWeight: 400,
-        textAlign: 'left',
-        whiteSpace: 'pre-wrap',
-        color: t.colors.gray[9],
       },
     },
-    '> .right-col': {
-      gap: 2,
+
+    '> .details-panel': {
+      padding: 0,
       width: '100%',
       display: 'flex',
-      gridColumn: 'span 2',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      [t.fn.smallerThan('lg')]: {
-        gridColumn: 'span 12',
-      },
-      '> .prod-progress-item': {
-        height: 34,
-        width: '100%',
-        borderRadius: 5,
+      position: 'relative',
+      gridColumn: 'span 11',
+      alignItems: 'stretch',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      '> .content': {
+        padding: '20px 30px',
       },
     },
   },
 });
 
-function ProdLine() {
+export function ProdLine({ item }: { item: any }) {
   const { classes } = Styles();
+  const [isHovered, setIsHovered] = React.useState(false);
+  return (
+    <Box
+      component={motion.div}
+      className={classes.prodLine}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Box className="main-panel">
+        <Box className="left-col">
+          <Box className="line-no" bg={getColorByStatus(item.status, true)}>
+            {item.no}
+          </Box>
+          <Text className="line-name">{item.name}</Text>
+        </Box>
+        <Box className="center-col">
+          <Box className="cc-item">
+            <Text className="cc-title">SİLİNENLER</Text>
+            <Text className="cc-value">3.460</Text>
+          </Box>
+          <Box className="cc-item">
+            <Text className="cc-title">KRİTER DIŞI</Text>
+            <Text className="cc-value">250</Text>
+          </Box>
+          <Box className="cc-item">
+            <Text className="cc-title">TOPLAM ÜRETİM</Text>
+            <Text className="cc-value">{item.production.reduce((a, b) => a + b.count, 0)}</Text>
+          </Box>
+          <Text className="cc-desc">CNC TORNA İŞLEME</Text>
+        </Box>
+        <Box className="right-col">
+          {item.production.map((prod, i) => (
+            <Box
+              className="prod-progress-item"
+              bg={getColorByStatus(prod.status)}
+              key={`prod-item${item.id}-progress-${i}`}
+              maw={`${getProdItemPercent(
+                prod.count,
+                item.production.reduce((a, b) => a + b.count, 0),
+              )}%`}
+            />
+          ))}
+        </Box>
+      </Box>
+      <AnimatePresence mode="wait">
+        {isHovered && (
+          <Box
+            component={motion.div}
+            className="details-panel"
+            transition={{ duration: 0.2 }}
+            exit={{ opacity: 0, height: 0 }}
+            key={`prod-item${item.id}-details`}
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+          >
+            <Box className="content">x</Box>
+          </Box>
+        )}
+      </AnimatePresence>
+    </Box>
+  );
+}
+
+export function ProdLines() {
+  const { classes } = Styles();
+
   return (
     <Box
       component={motion.div}
@@ -326,43 +293,8 @@ function ProdLine() {
       animate={{ opacity: 1, scale: 1 }}
       className={classes.prodLineCont}
     >
-      {data.map((item) => (
-        <Box key={`prod-line-${item.id}`} className={classes.prodLine}>
-          <Box className="left-col">
-            <Box className="line-no" bg={getColorByStatus(item.status, true)}>
-              {item.no}
-            </Box>
-            <Text className="line-name">{item.name}</Text>
-          </Box>
-          <Box className="center-col">
-            <Box className="cc-item">
-              <Text className="cc-title">SİLİNENLER</Text>
-              <Text className="cc-value">3.460</Text>
-            </Box>
-            <Box className="cc-item">
-              <Text className="cc-title">KRİTER DIŞI</Text>
-              <Text className="cc-value">250</Text>
-            </Box>
-            <Box className="cc-item">
-              <Text className="cc-title">TOPLAM ÜRETİM</Text>
-              <Text className="cc-value">{item.production.reduce((a, b) => a + b.count, 0)}</Text>
-            </Box>
-            <Text className="cc-desc">CNC TORNA İŞLEME</Text>
-          </Box>
-          <Box className="right-col">
-            {item.production.map((prod, i) => (
-              <Box
-                className="prod-progress-item"
-                bg={getColorByStatus(prod.status)}
-                key={`prod-item${item.id}-progress-${i}`}
-                maw={`${getProdItemPercent(
-                  prod.count,
-                  item.production.reduce((a, b) => a + b.count, 0),
-                )}%`}
-              />
-            ))}
-          </Box>
-        </Box>
+      {PodMockProdLine.map((item) => (
+        <ProdLine key={`prod-line-${item.id}`} item={item} />
       ))}
     </Box>
   );
@@ -372,32 +304,32 @@ const Tabs = [
   {
     label: 'İşlemdekiler',
     value: 'in-progress',
-    view: ProdLine,
+    view: ProdLines,
   },
   {
     label: 'Beklemede',
     value: 'pending',
-    view: ProdLine,
+    view: ProdLines,
   },
   {
     label: 'Kontrolde',
     value: 'in-check',
-    view: ProdLine,
+    view: ProdLines,
   },
   {
     label: 'Tamamlanan',
     value: 'completed',
-    view: ProdLine,
+    view: ProdLines,
   },
   {
     label: 'Durdurulan',
     value: 'stopped',
-    view: ProdLine,
+    view: ProdLines,
   },
   {
     label: 'İptal Edilen',
     value: 'canceled',
-    view: ProdLine,
+    view: ProdLines,
   },
 ];
 
