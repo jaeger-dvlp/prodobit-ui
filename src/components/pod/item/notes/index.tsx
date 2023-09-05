644e4fce443ea2c919322c7aa40c287925688b0e
JAEGER_ <omer.kayalar0101@icloud.com>
2023-09-05 13:26:55 +0300

    feat: init notes page

---
+++ src/components/pod/item/notes/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ProdobitAppTheme as t } from '@/theme';
+import PodItemsNavbar from '@/components/pod/navbar';
+import { Box, Title, createStyles } from '@mantine/core';
+
+type Props = {
+  item: any;
+};
+
+const styles = createStyles({
+  root: {
+    gap: 60,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  pageHeading: {
+    fontWeight: 500,
+    fontSize: '31px',
+    paddingBottom: 30,
+    color: t.colors.gray[9],
+    borderBottom: `1px solid rgba(0, 0, 0, 0.20)`,
+  },
+});
+
+function PaProductionItemNotesContent({ item }: Props) {
+  const { classes } = styles();
+  return (
+    <Box className={classes.root}>
+      <PodItemsNavbar />
+      <Title className={classes.pageHeading} order={1}>
+        Üretim Notları
+      </Title>
+    </Box>
+  );
+}
+
+export default PaProductionItemNotesContent;
