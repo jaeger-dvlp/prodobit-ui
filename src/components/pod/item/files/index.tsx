d32a08bdc7aed2ad8d477ce8a79385cfb97bd1af
JAEGER_ <omer.kayalar0101@icloud.com>
2023-09-14 11:18:04 +0300

    feat: image category selectors

--- src/components/pod/item/files/index.tsx
+++ src/components/pod/item/files/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ProdobitAppTheme as t } from '@/theme';
 import { Box, Button, Text, Title, createStyles } from '@mantine/core';
 import { ImageIcon, PenToolIcon } from '@/components/icons';
+import { getRandomUUID } from '@/common/utils/misc';
 
 type Props = {
   item: any;
@@ -68,10 +69,140 @@ const styles = createStyles({
   },
 });
 
+const imageCategories = [
+  {
+    slug: 'all',
+    id: getRandomUUID(),
+    name: 'Tüm\nGörseller',
+  },
+  {
+    slug: 'product',
+    id: getRandomUUID(),
+    name: 'Kategori\nAdı',
+  },
+  {
+    slug: 'product2',
+    id: getRandomUUID(),
+    name: 'Kategori\nAdı',
+  },
+  {
+    slug: 'product3',
+    id: getRandomUUID(),
+    name: 'Kategori\nAdı',
+  },
+  {
+    slug: 'product4',
+    id: getRandomUUID(),
+    name: 'Kategori\nAdı',
+  },
+];
+
+const InnerStyles = createStyles({
+  imagesRoot: {
+    gap: 30,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  imagesCategories: {
+    gap: 10,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    '> .category-btn': {
+      opacity: 0.5,
+      border: 'none',
+      height: 'auto',
+      borderRadius: 100,
+      color: t.colors.blue[5],
+      transition: 'all 0.2s ease',
+      backdropFilter: 'blur(74px)',
+      padding: '10px 10px 20px 10px',
+      backgroundColor: 'rgba(255, 255, 255, 0.40)',
+      boxShadow:
+        '0px 18.26189px 22.82736px 0px rgba(0, 0, 0, 0.05), -0.76091px 0.76091px 0.76091px -1.52182px rgba(255, 255, 255, 0.35) inset, 0px 0.76091px 6.0873px 0px rgba(255, 255, 255, 0.35) inset',
+      '> div > span': {
+        gap: 11,
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        '> .category-btn-index-container': {
+          display: 'flex',
+          fontWeight: 500,
+          minWidth: '62px',
+          fontSize: '18px',
+          minHeight: '62px',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        '> .category-btn-name': {
+          fontWeight: 400,
+          fontSize: '12px',
+          textAlign: 'center',
+          whiteSpace: 'pre-wrap',
+          color: t.colors.blue[3],
+          transition: 'all 0.2s ease',
+        },
+      },
+      "&[data-selected-category='true']": {
+        opacity: 1,
+        '& .category-btn-name': {
+          color: t.colors.blue[7],
+        },
+      },
+    },
+  },
+});
+
+function InnerImagesView() {
+  const { classes } = InnerStyles();
+  const [selectedImageCategory, setSelectedImageCategory] = React.useState<string>('all');
+
+  return (
+    <Box className={classes.imagesRoot}>
+      <Box className={classes.imagesCategories}>
+        {imageCategories.map((category, i) => (
+          <Button
+            key={category.id}
+            variant="default"
+            className="category-btn"
+            onClick={() => setSelectedImageCategory(category.slug)}
+            data-selected-category={selectedImageCategory === category.slug}
+          >
+            <Box className="category-btn-index-container">
+              <Text>{i + 1 >= 10 ? i + 1 : `0${i + 1}`}</Text>
+            </Box>
+            <Text className="category-btn-name">{category.name}</Text>
+          </Button>
+        ))}
+      </Box>
+    </Box>
+  );
+}
+
+function InnerTechnicalDrawingsView() {
+  const { classes } = InnerStyles();
+
+  return <Box>Technical Drawings</Box>;
+}
+
+const innerViewByCategory = {
+  images: InnerImagesView,
+  'technical-drawings': InnerTechnicalDrawingsView,
+};
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function PaProductionItemFilesContent({ item }: Props) {
   const { classes } = styles();
   const [selectedFilesCategory, setSelectedFilesCategory] = React.useState<string>('images');
+
+  const InnerView = innerViewByCategory[selectedFilesCategory];
 
   return (
     <Box className={classes.root}>
@@ -100,6 +231,7 @@ function PaProductionItemFilesContent({ item }: Props) {
           </Button>
         </Box>
       </Box>
+      <InnerView key={`inner-view-${selectedFilesCategory}`} />
     </Box>
   );
 }
