087226d118428c4b88c450ab3e408ea56b5ff6e9
JAEGER_ <omer.kayalar0101@icloud.com>
2023-09-19 13:49:34 +0300

    feat: export new icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -352,6 +352,21 @@ function CustomChevrons(props: CustomIconProps) {
   );
 }
 
+function CustomChevronRight(props: CustomIconProps) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...DefaultProps} {...props}>
+      <path
+        d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
 function DownloadCloudIcon(props: CustomIconProps) {
   return (
     <svg viewBox="0 0 14 13" fill="none" {...DefaultProps} {...props}>
@@ -2336,6 +2351,7 @@ export {
   CustomChevronDown,
   CustomChevronUp,
   CustomChevrons,
+  CustomChevronRight,
   DownloadCloudIcon,
   UploadCloudIcon,
   CustomFilterBarsIcon,
